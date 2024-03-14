2d2e8caeb73bf1eaf46864a26e2170709b850891
Aditya Babar <70708282+adityababar07@users.noreply.github.com>
2024-03-14 08:43:49 +0000

    just added new and old versions of ceaser cipher

---
+++ new/ceasercipher.py
@@ -0,0 +1,26 @@
+from encryption import encrypt
+from decryption import decrypt
+
+
+while True:
+
+    print('''What do you want to do ?
+
+                1)Encrypt 
+                2)Decrypt
+
+            press what you want to do!!!!!
+        ''')
+
+    choice = int(input("enter what is your choice:\t")) 
+
+    if choice == 1:
+
+        text = input("Enter your text:\t")
+        encrypt(text)
+
+
+    elif choice == 2:
+
+        text = input("enter your text :\t")
+        decrypt(text)
